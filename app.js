21a77d4177ff93338d434e431fbe4fca285c0097
dheeraj303 <singh.dheeraj6303@gmail.com>
2022-12-23 11:14:17 +0530

    basic router

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 
 const express=require('express');
 const app=express();
-
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next();
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="product"><input type="text" name="size"><button type="submit">Send</button></form>')
 });
-app.use((req,res,next)=>{
-    console.log('In another middleware');
+app.use('/product',(req,res,next)=>{
+    console.log(req.body.product);
+    console.log(req.body.size);
+    res.redirect('/');
+});
+app.use('/',(req,res,next)=>{
+    // console.log('In another middleware');
     res.send('<h1>Hello World</h1>')
 });
 app.listen(4000);
